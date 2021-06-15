5d640c0cf4970f254007bcabf8c67fc76a7bad3f
Roberto <rrojasj@ucenfotec.ac.cr>
2021-06-15 15:49:20 -0600

    Lovelace_myplace_lp

---
+++ js/index.js
@@ -0,0 +1,173 @@
+'use strict';
+//Menu
+let headerContenedor = document.querySelector('#header');
+let botonActivador = document.querySelector('#btn-menuPricipal');
+
+botonActivador.addEventListener('click', () => {
+    headerContenedor.classList.toggle('Activado')
+});
+
+
+//Header con clase fixed
+window.onscroll = function() { funcionScroll() };
+let header = document.querySelector('#header');
+let headerFixed = header.offsetTop;
+
+const funcionScroll = () => {
+    if (window.pageYOffset > headerFixed) {
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    }
+};
+
+
+
+
+//Slider
+let sliderIndice = 0;
+
+const funcionSlider = () => {
+    let i;
+    let slides = document.getElementsByClassName('slide');
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    sliderIndice++;
+    if (sliderIndice > slides.length) { sliderIndice = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[sliderIndice - 1].style.display = "block";
+
+    setTimeout(funcionSlider, 8000);
+};
+funcionSlider();
+
+//Validacion de Contactenos
+const btn_enviar = document.querySelector('#btn-contactenos');
+const txt_nombre = document.querySelector('#txt-contactenosnombre');
+const txt_correo = document.querySelector('#txt-contactenoscorreo');
+const txt_mesaje = document.querySelector('#txt-contactenosMensaje');
+
+let validar = () => {
+    let inputs_requeridos = document.querySelectorAll('#frm-contacto [required]');
+    let regexCorreo = /^[a-zA-Z.0-9]+\@{1}[a-zA-Z.]+$/;
+    //let regextelefono = /^(|\d{3})(-|\s)?(\d{4})(-|\s)?(\d{4})$/;
+    let error = false;
+
+    //Ini un array con todos los objetos de clase required (todos los campos del form)
+    //Valida que no esten en blanco
+    for (let i = 0; i < inputs_requeridos.length; i++) {
+        if (inputs_requeridos[i].value == '') {
+            inputs_requeridos[i].classList.add('error');
+            error = true;
+        } else {
+            inputs_requeridos[i].classList.remove('error');
+        }
+    }
+
+    //Ini Validacion campo Correo
+    if (regexCorreo.test(txt_correo.value) == false) {
+        error = true;
+        txt_correo.classList.add('error');
+    } else {
+        txt_correo.classList.remove('error');
+    }
+    //fin Validacion campo Correo
+
+    return error;
+};
+
+let limpiar = () => {
+    txt_nombre.value = "";
+    txt_correo.value = "";
+    txt_mesaje.value = "";
+};
+
+let obtener_datos = () => {
+    let error = validar();
+
+    if (error) {
+        Swal.fire({
+            'title': 'El formulario no se pudo enviar',
+            'text': 'Por favor revise los campos resaltados',
+            'icon': 'warning'
+        });
+    } else {
+        console.log(txt_nombre.value);
+        console.log(txt_correo.value);
+        console.log(txt_mesaje.value);
+
+        Swal.fire({
+            'title': 'Proceso realizado con éxito',
+            'text': 'Sus datos se enviaron adecuadamente',
+            'icon': 'success'
+        }).then(() => {
+            limpiar();
+        });
+    }
+};
+
+btn_enviar.addEventListener('click', obtener_datos);
+
+//Validacion de Contactenos
+
+
+// Modal
+let modal1 = document.querySelector('#modal1');
+let botonModal1 = document.querySelector('#patrocinador1')
+let cerrar1 = document.querySelector("#modal1 .close");
+let modal2 = document.querySelector('#modal2');
+let botonModal2 = document.querySelector('#patrocinador2')
+let cerrar2 = document.querySelector("#modal2 .close");
+let modal3 = document.querySelector('#modal3');
+let botonModal3 = document.querySelector('#patrocinador3')
+let cerrar3 = document.querySelector("#modal3 .close");
+let modal4 = document.querySelector('#modal4');
+let botonModal4 = document.querySelector('#patrocinador4')
+let cerrar4 = document.querySelector("#modal4 .close");
+let modal5 = document.querySelector('#modal5');
+let botonModal5 = document.querySelector('#patrocinador5')
+let cerrar5 = document.querySelector("#modal5 .close");
+
+botonModal1.addEventListener('click', () => {
+    modal1.style.display = 'block';
+});
+
+cerrar1.addEventListener('click', () => {
+    modal1.style.display = 'none';
+});
+
+botonModal2.addEventListener('click', () => {
+    modal2.style.display = 'block';
+});
+
+cerrar2.addEventListener('click', () => {
+    modal2.style.display = 'none';
+});
+
+botonModal3.addEventListener('click', () => {
+    modal3.style.display = 'block';
+});
+
+cerrar3.addEventListener('click', () => {
+    modal3.style.display = 'none';
+});
+
+botonModal4.addEventListener('click', () => {
+    modal4.style.display = 'block';
+});
+
+cerrar4.addEventListener('click', () => {
+    modal4.style.display = 'none';
+});
+
+botonModal5.addEventListener('click', () => {
+    modal5.style.display = 'block';
+});
+
+cerrar5.addEventListener('click', () => {
+    modal5.style.display = 'none';
+});
